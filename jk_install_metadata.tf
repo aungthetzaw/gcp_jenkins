fb1df7801075f209e9b0112321b2b2d2a7b46248
aungthetzaw <aungthetzaw93@gmail.com>
2023-01-07 19:31:06 +0700

    Create jk_install_metadata.tf

---
+++ jk_install_metadata.tf
@@ -0,0 +1,14 @@
+# Bootstrapping Script to install jenkins
+data "template_file" "jk-metadata" {
+  template = <<EOF
+sudo yum update -y
+sudo hostnamectl set-hostname jenkins-server
+sudo yum -y install java-11-openjdk-devel
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo yum -y install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+EOF
+}
